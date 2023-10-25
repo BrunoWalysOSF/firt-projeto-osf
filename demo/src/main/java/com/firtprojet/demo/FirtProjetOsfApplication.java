25800f9abab5c4ad5232d0c3e75213c14e26bef0
Bruno <bruno.silva.ifactory@cnpbrasil.com.br>
2023-10-25 20:17:38 -0300

    cors erro

--- demo/src/main/java/com/firtprojet/demo/FirtProjetOsfApplication.java
+++ demo/src/main/java/com/firtprojet/demo/FirtProjetOsfApplication.java
@@ -2,12 +2,15 @@ package com.firtprojet.demo;
 
 
 import com.firtprojet.demo.service.OrdersService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default Server Url")})
 public class FirtProjetOsfApplication {
 
 	public static void main(String[] args) {
